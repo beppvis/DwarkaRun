4f94b2c2c31555059f3919e1e207a8cd5a121a4a
Aadithyaa Eeswaran <eaadithyaa@gmail.com>
2023-10-31 04:04:34 +0530

    SPRITE MFFFF

--- engine/src/main/java/com/dwarakarun/engine/WindowSystem.java
+++ engine/src/main/java/com/dwarakarun/engine/WindowSystem.java
@@ -5,22 +5,34 @@ import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 import org.lwjgl.system.*;
 
-import java.io.File;
-import java.io.IOException;
 import java.nio.*;
 import java.nio.file.*;
+import java.util.*;
 
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL33.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.stb.STBImage.*;
+
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
 
 public class WindowSystem extends GameSystem {
 	private long handle;
-	private int vbo;
-	private int vao;
-	private int shader;
+	private int tex;
+
+	private void handleErrors() {
+		int err = glGetError();
+		while (err != GL_NO_ERROR) {
+			System.out.format("ERROR: %d\n", err);
+			err = glGetError();
+		}
+	}
 
 	public WindowSystem(Engine eng) {
 		super(eng);
@@ -48,6 +60,83 @@ public class WindowSystem extends GameSystem {
 		glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 		System.out.println("Init windowsystem");
 		glLoadIdentity();
+		handleErrors();
+
+		tex = glGenTextures();
+		glEnable(GL_TEXTURE_2D);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, tex);
+
+		handleErrors();
+
+		String spriteStr = "/home/n3rdy/dev/java/DwarakaRun/sprite.png";
+
+//		BufferedImage bufimg = null;
+//		try {
+//			bufimg = ImageIO.read(new File(spriteStr));
+//		} catch(IOException e) {
+//			System.out.println(e);
+//		} finally {
+//		}
+//		int[] pixels = bufimg.getRGB(0, 0, bufimg.getWidth(), bufimg.getHeight(), null, 0, bufimg.getWidth());
+//		ByteBuffer buffer = ByteBuffer.allocateDirect(pixels.length * 4);
+//		for (int pixel : pixels) {
+//			buffer.put((byte) ((pixel >> 16) & 0xFF));
+//			buffer.put((byte) ((pixel >> 8) & 0xFF));
+//			buffer.put((byte) (pixel & 0xFF));
+//			buffer.put((byte) ((pixel >> 24) & 0xFF));
+//		}
+//		buffer.flip();
+
+
+		ByteBuffer image;
+        int width, height, channels;
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            /* Prepare image buffers */
+            IntBuffer w = stack.mallocInt(1);
+            IntBuffer h = stack.mallocInt(1);
+            IntBuffer comp = stack.mallocInt(1);
+
+            /* Load image */
+            stbi_set_flip_vertically_on_load(true);
+            image = stbi_load(spriteStr, w, h, comp, 4);
+            if (image == null) {
+                throw new RuntimeException("Failed to load a texture file!"
+                                           + System.lineSeparator() + stbi_failure_reason());
+            }
+
+            /* Get width and height of image */
+            width = w.get();
+            height = h.get();
+			channels = comp.get();
+        }
+
+		System.out.println("Loaded img");
+		System.out.format("Width: %d\tHeight: %d\tChannels: %d\t\n", width, height, channels);
+
+		if (image.limit() != width*height*4) {
+			System.out.println("WEIRD SIZE");
+		} else {
+			System.out.println("size sane");
+		}
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		handleErrors();
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		handleErrors();
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		handleErrors();
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		handleErrors();
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+		handleErrors();
+
+//		glGenerateMipmap(GL_TEXTURE_2D);
+		handleErrors();
 }
 
 	public void clear() {
@@ -74,14 +163,23 @@ public class WindowSystem extends GameSystem {
 		if (glfwWindowShouldClose(handle)) {
 			eng.end();
 		}
+		glBindTexture(GL_TEXTURE_2D, tex);
 		glPushMatrix();
 		glTranslatef(-0.25f, -0.25f, 0);
-		glColor3f(0.5f, 0.5f, 0.4f);
 		glBegin(GL_QUADS);
-		glVertex2f(0 ,0);
+
+		glTexCoord2f(0.0f, 0.0f);
+		glVertex2f(0, 0);
+
+		glTexCoord2f(0, 1f);
 		glVertex2f(0, 0.5f);
+
+		glTexCoord2f(1f, 1f);
 		glVertex2f(0.5f, 0.5f);
+
+		glTexCoord2f(1f, 0);
 		glVertex2f(0.5f, 0);
+
 		glEnd();
 		glPopMatrix();
 	}
